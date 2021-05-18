28c6a7c2a79477c753aec847939601cffc54df26
Philani Ncube <ncbphi001@gmail.com>
2021-05-18 19:17:08 +0200

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        This is an app to find out about the latest events happening in your
+        area
+      </p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+}
